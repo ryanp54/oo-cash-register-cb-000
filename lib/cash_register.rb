144a9e6a8379232168560a6f1a2b7910d26bacbd
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-09 06:10:01 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
     self.total += price * quantity
   end
   def apply_discount
-    self.total = self.total - self.discount)
+    self.total = self.total - self.discount
     self.discount == 0 ? "There is no discount to apply" : "Discount applied. Total is #{total}"
   end
   def void_last_transaction
