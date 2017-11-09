a6e9326f5180cdcb92db3c8875a1df992913a3a5
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-09 06:05:48 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
-
+class CashRegister
+  attr_accessor :total, :discount, :previous_total, :items
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  def add_item(title, price, quantity = 1)
+    items << title
+    previous_total = total
+    total += price * quantity
+  end
+  def apply_discount
+    total = total * (1 - discount)
+    discount == 0 ? "No discount" : "Discount applied. Total is #{total}"
+  end
+  def void_last_transaction
+    total = previous_total
+    items.pop()    
+  end
+end
